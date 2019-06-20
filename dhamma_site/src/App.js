143c4f3c7411daeaea2764bdefdda69bbe84bc93
Harini Arnolda <harini.arnolda@pearson.com>
2019-06-20 13:31:43 +0530

    Project setup

---
+++ dhamma_site/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import MenuBar from './components/navBar/MenuBar.jsx';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Sri Kalyani Yogasrama Samstha</h2>
+        </div>
+        <MenuBar/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
